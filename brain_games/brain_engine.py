da73aa66376e6f63b592afbc45cc4e63e20753d1
churakov.anton@gmail.com <root@DESKTOP-KSDNFD8.localdomain>
2023-01-03 21:47:21 +0500

    fix pyproject.toml, brain_engine, brain_even, makefile

--- brain_games/brain_engine.py
+++ brain_games/brain_engine.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
 
 import prompt
-from random import randint
+quantity_of_correct_answers = 3
 
 
-def welcome_user():
+def launch_game(game):
     print('Welcome to the Brain Games!')
-
-
-def name_of_gamer():
-    global name_of_gamer
     name_of_gamer = prompt.string('May I have your name? ')
     print(f'Hello, {name_of_gamer}!')
-    return name_of_gamer
+    print(game.rules_of_game)
 
-
-number_random_range = 99
-
-
-def question():
-    number_from_question = randint(1, number_random_range)
-    print(f'Question: {number_from_question}')
-    return number_from_question
-
-
-def answer():
-    answer_of_gamer = prompt.string('Your answer: ')
-    return answer_of_gamer
-
-
-def congrats():
-    print(f'Congratulations, {name_of_gamer}!')
+    counter = 0
+    for _ in range(quantity_of_correct_answers):
+        correct_answer = game.question()
+        gamer_answer = prompt.string('Your answer: ')
+        if gamer_answer == correct_answer:
+            print('Correct!')
+            counter += 1
+        else:
+            print(f"'{gamer_answer}' is wrong answer ;(. \
+Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name_of_gamer}!")
+            return
+    if counter == 3:
+        print(f'Congratulations, {name_of_gamer}!')
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.brain_engine import welcome_user
-from brain_games.brain_engine import name_of_gamer
-from brain_games.brain_engine import question
-from brain_games.brain_engine import answer
+from brain_games.brain_engine import launch_game
+from brain_games.games import iseven
 
 
 def main():
-    welcome_user()
-    gamer_name = name_of_gamer()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    quantity_of_correct_answers = 3
-    counter = 0
-
-    for _ in range(quantity_of_correct_answers):
-        question_for_gamer = question()
-        correct_answer = question_for_gamer % 2 == 0 and 'yes' or 'no'
-        gamer_answer = answer()
-        if gamer_answer == correct_answer:
-            print('Correct!')
-            counter += 1
-        else:
-            print(f"'{gamer_answer}' is wrong answer ;(. \
-Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {gamer_name}!")
-            return
-    if counter == 3:
-        print(f'Congratulations, {gamer_name}!')
+    launch_game(iseven)
 
 
 if __name__ == '__main__':
