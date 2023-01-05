386df13cb3c2af8aa443da318252567343ef4c63
churakov.anton@gmail.com <root@DESKTOP-KSDNFD8.localdomain>
2023-01-06 00:55:51 +0500

    add progression.py & brain_progression.py

---
+++ brain_games/games/progression.py
@@ -0,0 +1,22 @@
+from random import randint
+length_of_progression = 10
+number_random_range = 99
+max_common_difference = 10
+rules_of_game = 'What number is missing in the progression?'
+
+
+def question():
+    start_element_of_progression = randint(0, number_random_range)
+    common_difference = randint(1, max_common_difference)
+    list_of_progression = []
+
+    for _ in range(length_of_progression):
+        list_of_progression.append(str(start_element_of_progression))
+        start_element_of_progression += common_difference
+
+    index_of_hided_element = randint(0, length_of_progression - 1)
+    correct_answer = list_of_progression[index_of_hided_element]
+    list_of_progression[index_of_hided_element] = '..'
+    list_as_string = ' '.join(list_of_progression)
+    print(f'Question: {list_as_string}')
+    return correct_answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.brain_engine import launch_game
+from brain_games.games import progression
+
+
+def main():
+    launch_game(progression)
+
+
+if __name__ == '__main__':
+    main()
